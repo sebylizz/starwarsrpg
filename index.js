a74a17b5154e9757f3ea78099592ba61b0882e91
sebylizz <sebastiankjandersen@gmail.com>
2023-09-24 23:26:25 +0200

    working build

--- index.js
+++ index.js
@@ -11,10 +11,9 @@ const pool = [{id: 0, color: "white"}]; // This will store the shared pool of ob
 app.use(express.static(path.join(__dirname, 'public')));
 
 wss.on('connection', (ws) => {
-    // Send the current object pool to the newly connected client.
     ws.send(JSON.stringify(pool));
 
-    // Handle messages from clients.
+ 
     ws.on('message', (message) => {
         switch (message.toString()){
             case "remove":
@@ -33,7 +32,6 @@ wss.on('connection', (ws) => {
                 }
         }
 
-        // Broadcast the updated pool to all connected clients.
         wss.clients.forEach((client) => {
             if (client.readyState === WebSocket.OPEN) {
                 client.send(JSON.stringify(pool));
